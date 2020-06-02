65c1ad603201df5879538acd42a72bd6d3469652
david movsesyan <coldseattle@mail.ru>
2020-06-02 20:37:57 +0300

    Updates

---
+++ precache-manifest.61211cfe83d7f21cfc2e56df1c427ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e53da922a5695242701b4ae102e14e",
+    "url": "/react-weather-app.github.io/index.html"
+  },
+  {
+    "revision": "37271bf8ee175b2fbfd2",
+    "url": "/react-weather-app.github.io/static/css/main.78fac8ab.chunk.css"
+  },
+  {
+    "revision": "2fccd81d2b05b3028725",
+    "url": "/react-weather-app.github.io/static/js/2.bba14630.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app.github.io/static/js/2.bba14630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37271bf8ee175b2fbfd2",
+    "url": "/react-weather-app.github.io/static/js/main.d540e0fe.chunk.js"
+  },
+  {
+    "revision": "f74b73dcfb1ea31e1b1b",
+    "url": "/react-weather-app.github.io/static/js/runtime-main.dc6ff394.js"
+  }
+]);
